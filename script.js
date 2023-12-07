7097b7296fd55b3e614a34da9d8cd2157cc18bdf
alexandermcmillan <mcmillancontact@protonmail.com>
2023-12-06 19:37:34 -0800

    render center image working

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const url = "https://thronesapi.com/api/v2/Characters"
 
-
+const characterList = document.querySelector("#character-list")
+const characterName = document.querySelector("#name")
+const familyName = document.querySelector("#family") 
 
 fetch(url)
 .then((resp) => resp.json())
 .then((data) => {
-  console.log(data);
+  // console.log(data);
+  // const li = document.createElement("li")
+  // li.textContent = data[30].fullName
   
+  const img = document.createElement("img")
+  img.src = data[3].imageUrl
+
+  characterName.textContent = data[3].fullName
+  familyName.textContent = data[3].family
+
+  // li.append(img)
+  characterList.append(img)
 })
